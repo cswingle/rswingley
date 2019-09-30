2cb0a2a7217d6961c911e73fc39a2a6ec1ab5b06
Christopher Swingley <cswingle@swingleydev.com>
2019-09-30 09:03:36 -0800

    add documentation

--- R/sfc_as_cols.R
+++ R/sfc_as_cols.R
@@ -1,3 +1,15 @@
+#' Add coordinates as new columns
+#'
+#' Extract the coordinates from the data frame and create two new columns
+#' containing these coordinates.
+#'
+#' @param x data frame containing an sf point geometry
+#' @param names character vector containing the names of the new columns to add
+#'
+#' @return data frame with the new columns added
+#' @export
+#' @examples
+#' geometry_layer %>% sfc_as_cols(names = c("lon", "lat"))
 sfc_as_cols <- function(x, names = c("x", "y")) {
     stopifnot(inherits(x, "sf") && inherits(sf::st_geometry(x), "sfc_POINT"))
     ret <- sf::st_coordinates(x)
