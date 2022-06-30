0e950283b9480346cada38ac91873aef15e2aed4
Christopher Swingley <cswingley@abrinc.com>
2022-06-30 11:23:15 -0800

    correct usage

--- R/print_all.R
+++ R/print_all.R
@@ -3,11 +3,12 @@
 #' Wraps the `print()` function with `n = Inf`
 #'
 #' @param x is the object to print
+#' @param ... arguments passed to `print()`
 #'
 #' @return prints its argument and returns it _invisibly_.
 #' @export
 #' @examples
-#' print_all(x, ...)
+#' print_all(tibble::as_tibble(mtcars))
 print_all <- function(x, ...) {
   print(x, n = Inf, ...)
 }
