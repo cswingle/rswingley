a37e6a707d318b7a1adcac6b4462aae6e1c6e7ad
Christopher Swingley <cswingley@abrinc.com>
2024-02-01 08:38:21 -0900

    don't traverse more than 16 links

--- R/link_path_original.R
+++ R/link_path_original.R
@@ -1,6 +1,7 @@
 #' Trace a symlink back to it's source file
 #'
 #' @param symlink Symbolic link to trace back
+#' @param count Number of recursive calls
 #'
 #' @return path to original file
 #' @export
@@ -10,7 +11,11 @@
 #' link_create("/tmp/a", "/tmp/b")
 #' link_create("/tmp/b", "/tmp/c")
 #' link_path_original("/tmp/c")
-link_path_original <- function(symlink) {
+link_path_original <- function(symlink, count = 0) {
+  count <- count + 1
+  if (count > 16) {
+    stop("Error: possible circular reference")
+  }
   symlink <- fs::path(symlink)
   if (fs::file_info(symlink)$type != "symlink") {
     return(symlink)
