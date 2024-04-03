c10dae13ab790158fa109d2229d97bd0846c6502
Christopher Swingley <cswingley@abrinc.com>
2024-04-03 08:05:19 -0800

    add count_prop function

---
+++ R/count_prop.R
@@ -0,0 +1,20 @@
+#' Get grouped count and proportion
+#'
+#' Similar to `count`, but also includes a `prop` column with the
+#' proportion for each group.
+#'
+#' @param x A data frame, data frame extension (tibble), or a lazy data frame.
+#' @param var Variables to group by.
+#' @param sort If ‘TRUE’, will show the largest groups at the top.
+#'
+#' @return An object of the same type as `.data`
+#' @export
+#' @examples
+#' library(dplyr)
+#' starwars |> count_prop(species)
+#' @importFrom rlang .data
+count_prop <- function(x, var, sort = FALSE) {
+  x |>
+    dplyr::count({{ var }}, sort = sort) |>
+    dplyr::mutate(prop = .data$n / sum(.data$n))
+}
